dced27e4d92b7c2a1cee05089622a44a62961f3c
jkryg3r <jakubkryger80@gmail.com>
2023-02-06 09:12:25 +0100

    gin
    sory

--- Assets/Scripts/Postac/Player.cs
+++ Assets/Scripts/Postac/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -58,5 +59,9 @@ public class Player : MonoBehaviour
         currentHealth -= damage;
 
         healthBar.SetHealth(currentHealth);
+        if(currentHealth == 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
     }
 }
