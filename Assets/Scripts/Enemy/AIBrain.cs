9307d6662bf963efb72c5471b0ee24600bd735f5
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2023-04-11 19:03:02 +0200

    Update AIBrain.cs

--- Assets/Scripts/Enemy/AIBrain.cs
+++ Assets/Scripts/Enemy/AIBrain.cs
@@ -35,7 +35,7 @@ public class AIBrain : MonoBehaviour
     public int dropHP;
     int summedDamage;
     float currentTime, currentTimeBloodPool;
-    float rotationSpeed = 0.175f;
+    float rotationSpeed = 0.15f;
     float aimAngle;
 
     RaycastHit hit;
