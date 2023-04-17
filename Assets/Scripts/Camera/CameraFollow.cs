f99d07458d409e7802e78c0f8b58a27d79d9ad15
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2023-04-17 18:03:53 +0200

    Poprawki UI i kamery.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -8,14 +8,16 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform player;
 
+    float maxScreenPoint = 0.25f;
     float smoothTime = 0.1f;
-    Vector3 offset = new Vector3(0f, 0f, -10f);
+    Vector3 offset = new Vector3(0f, -0.5f, -10f);
     Vector3 velocity = Vector3.zero;
     Vector3 targetPosition;
 
-    void LateUpdate()
+    void Update()
     {
-        targetPosition = player.position + offset;
+        Vector3 mousePos = Input.mousePosition * maxScreenPoint + new Vector3(Screen.width, Screen.height, 0f) * ((1f - maxScreenPoint) * 0.5f);
+        targetPosition = (player.position + Camera.main.ScreenToWorldPoint(mousePos)) / 2f + offset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
