fbf44a9b42af497aa12a8eb5c68d6878cba12502
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2023-04-22 23:05:35 +0200

    Poprawki.

--- Assets/Scripts/Camera/WidescreenNotification.cs
+++ Assets/Scripts/Camera/WidescreenNotification.cs
@@ -7,6 +7,6 @@ public class WidescreenNotification : MonoBehaviour
     void Start()
     {
         if ((float)Screen.width / (float)Screen.height > (21f/9f)+.1f || (float)Screen.width / (float)Screen.height < (4f / 3f) - .1f)
-            GameObject.FindWithTag("HealthBar").GetComponent<HealthBar>().MessageBox("WARNING! This game does not fully support aspect ratios over 21:9 or under 4:3!");
+            GameObject.FindWithTag("HealthBar").GetComponent<HealthBar>().MessageBox("WARNING: This game does not fully support aspect ratios over 21:9 or under 4:3!");
     }
 }
