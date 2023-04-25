d011fa11656c1ca6af3f42a999da14c090b57642
KrzysiekKropka <91200856+KrzysiekKropka@users.noreply.github.com>
2023-04-25 12:09:06 +0200

    Poprawki kamery

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -9,7 +9,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform player;
 
     float maxScreenPoint = 0.6f;
-    float smoothTime = 0.1f;
+    float smoothTime = 0.125f;
     Vector3 offset = new Vector3(0f, 0f, -10f);
     Vector3 velocity = Vector3.zero;
     Vector3 targetPosition;
